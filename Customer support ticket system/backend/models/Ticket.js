23caab43b7bb29c72f9702f20374d7af73927b60
Nikhil Singh Chouhan <chouhannikhil203@gmail.com>
2026-08-02 15:18:00 +0530

    Add files via upload

---
+++ Customer support ticket system/backend/models/Ticket.js
@@ -0,0 +1,38 @@
+const mongoose = require("mongoose");
+
+const ticketSchema = new mongoose.Schema({
+
+    title:{
+        type:String,
+        required:true
+    },
+
+    description:{
+        type:String,
+        required:true
+    },
+
+    category:{
+        type:String,
+        required:true
+    },
+
+    status:{
+        type:String,
+        default:"Open"
+    },
+    priority: {
+    type: String,
+    required: true
+},
+
+    // createdBy:{
+    //     type:String,
+    //     required:true
+    // }
+
+},{
+    timestamps:true
+});
+
+module.exports = mongoose.model("Ticket",ticketSchema);
